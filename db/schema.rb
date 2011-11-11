7ad81187edb770e31920ccfdb5e85ea37419f113
Steve Halladay <shallada@gmail.com>
2011-11-11 15:15:31 -0700

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111111170958) do
+ActiveRecord::Schema.define(:version => 20111111184729) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "documents", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "asset_url"
+    t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
