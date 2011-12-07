79c903477eca8d0cdebeae5fa59df81fe2b82ee6
Tyler Garlick <tjgarlick@gmail.com>
2011-12-07 13:47:25 -0700

    deleted a whole bunch of unnecessary files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111201195114) do
+ActiveRecord::Schema.define(:version => 20111207174353) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(:version => 20111201195114) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "customer_document_reference"
-    t.integer  "project_requirement_type_id"
     t.integer  "person_id"
     t.integer  "prerequisite_id"
   end
@@ -166,7 +165,9 @@ ActiveRecord::Schema.define(:version => 20111201195114) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end  create_table "users", :force => true do |t|
+  end
+
+  create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
